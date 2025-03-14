8225d08bae9ccf216d01c41ebf7e5586fb5dbe4b
Fauzan <fauzantahir660@gmail.com>
2025-03-14 17:24:45 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// })
+
+
+// var page = document.querySelector("#page-1");
+
+// gsap.to(page, {
+//   scale:0.5,
+//   duration: 3
+// });
+
+
+// gsap.from(page, {
+//   yPercent: 100,
+//   duration: 2,
+//   scale:0.5
+// });
+
+// gsap.to(page, {
+//   yPercent: 30,
+//   duration: 2
+// });
+
+// gsap.to(page, {
+//     delay:3,
+//     rotation: 360,
+//     duration: 1,
+//     scale: 1
+// });
+
+// gsap.to(page, {
+//     delay: 3,
+//     yPercent: 0,
+//     duration: 1
+// });
+
